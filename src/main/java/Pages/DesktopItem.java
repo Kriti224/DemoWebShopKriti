3002d4212390fc4e0202a422ab5755f6935137d4
kriti.prasad <prasadkriti9@gmail.com>
2022-07-11 03:42:21 -0400

    assignment_Kriti_DemoWebTest

---
+++ src/main/java/Pages/DesktopItem.java
@@ -0,0 +1,26 @@
+/*package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class DesktopItem extends SeleniumCommands{
+
+    public DesktopItem(WebDriver driver) {
+        super(driver);
+    }
+
+    @Override
+    public By scopedLocator(){
+        return  By.xpath("");
+    }
+
+    private By addCart = By.xpath("");
+
+    public void addToCart(){
+        click(addCart);
+    }
+
+
+}
+
+ */
---
+++ src/main/java/Pages/SeleniumCommands.java
@@ -0,0 +1,59 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public  class SeleniumCommands {
+    public WebDriver driver;
+
+    public SeleniumCommands(WebDriver driver)
+    {
+        this.driver = driver;
+    }
+/*
+    public abstract By scopedLocator();
+
+    public WebElement scopedElement = null;
+
+    */
+
+    public WebElement findElement(By by){
+
+        WebDriverWait wait = new WebDriverWait(driver,5);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    //clicks an element
+    public void click(By by)
+    {
+        findElement(by).click();
+    }
+
+    //sends keys
+    public void sendKeys(By by,String value)
+    {
+        findElement(by).sendKeys(value);
+    }
+
+    //selects from dropdown
+    public void selectFromDropdown(By by,String value)
+    {
+        var select=new Select(findElement(by));
+        select.selectByVisibleText(value);
+    }
+
+    //navigates to url
+    public void navigateToUrl(String url)
+    {
+        driver.navigate().to(url);
+    }
+
+    public void quitBrowser()
+    {
+        driver.quit();
+    }
+}
